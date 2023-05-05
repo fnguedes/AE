6fabce9fee3dc2100c13e98d0098f5d84ae72005
fnguedes <tututs0@hotmail.com>
2023-05-05 11:23:35 -0300

    PostIT concluído com pedência de design

--- src/Style/stylePostIt.js
+++ src/Style/stylePostIt.js
@@ -1,23 +1,71 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 export default StyleSheet.create({
-    container:{
-        width:70,
-        height:70,
-        backgroundColor:"#00f",
-        borderRadius:40,
-        justifyContent:'center',
-        alignItems:'center'
+    container: {
+        width: '90%',
+        height: Dimensions.get('screen').height / 20,
+        backgroundColor: "#5ED2E3",
+        borderRadius: 40,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 30,
+        flexDirection: 'row',
+        
     },
-    containerSigla:{
-        width:55,
-        height:55,
-        backgroundColor:"#f0f",
+    desenhoContainerTotal:{
+        width:'80%',
+        height:'95%',
         borderRadius:40,
-        justifyContent:'center',
-        alignItems:'center'
+        borderWidth:1,
+        justifyContent:'flex-start',
+        alignItems:'center',
+        flexDirection:'row',
+        backgroundColor:'#0ff'
     },
-    txtSigla:{
-        fontSize:18
+    containerContainerSigla: {
+        height: '125%',
+        width: '20%',
+        backgroundColor: "#181818",
+        borderRadius: 500,
+        borderTopLeftRadius:0,
+        borderBottomLeftRadius:0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+    },
+    containerSigla: {
+        width: '70%',
+        height: '87%',
+        backgroundColor: "#5ED2E3",
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    desenhoContainer: {
+        width: "95%",
+        height: '95%',
+        borderRadius: 500,
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    txtSigla: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    containerInfo: {
+        width: '73%',
+        height: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginLeft: '2%',
+    },
+    txtLocal: {
+        fontSize: 14,
+        fontWeight:'500'
+    },
+    txtHora: {
+        fontSize: 14,
     }
 })
--- src/Style/styleWeek.js
+++ src/Style/styleWeek.js
@@ -3,7 +3,8 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+        justifyContent:'flex-start',
+        alignItems:'center',
+        backgroundColor:'#181818'
     }
 })
