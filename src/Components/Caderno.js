70e5c24b7997080328ed87ab59f460f843bb31ca
fnguedes <tututs0@hotmail.com>
2023-05-15 09:35:18 -0300

    Criação dos componentes principais da telas inicais

---
+++ src/Components/Caderno.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function Caderno(){
+  return(
+    <View>
+      <View>
+        <View></View>
+        <View></View>
+        <View></View>
+      </View>
+      <View>
+        
+      </View>
+    </View>
+  )
+}
