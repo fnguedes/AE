3ad8a3586b05660809677028b1fcb3aa60145e9e
fnguedes <tututs0@hotmail.com>
2023-05-16 11:29:07 -0300

    Componente Caderno iniciado

--- src/Components/Caderno.js
+++ src/Components/Caderno.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { View } from "react-native";
 
-export default function Caderno(){
-  return(
-    <View>
-      <View>
-        <View></View>
-        <View></View>
-        <View></View>
+export default function Caderno() {
+  return (
+    <View style={style.container} >
+      <View style={style.containerTop} >
+        <View style={style.sigla} ></View>
+        <View style={style.titulo} ></View>
+        <View style={style.lixo} ></View>
       </View>
-      <View>
-        
+      <View style={style.containerDescricao} >
+        <View style={style.descricao}></View>
       </View>
     </View>
   )
---
+++ src/Components/Style/styleCaderno.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container:{
+    
+  },
+  containerTop:{
+
+  },
+  sigla:{
+
+  },
+  titulo:{
+
+  },
+  lixo:{
+
+  },
+  containerDescricao:{
+
+  },
+  descricao:{
+
+  }
+})
